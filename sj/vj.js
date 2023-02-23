630757d24983607de4305697044ae666eeb8f882
ssssojung <1216thwjd@naver.com>
2023-02-23 15:11:22 +0900

    스와이퍼 버튼 커스터마이징

--- sj/vj.js
+++ sj/vj.js
@@ -1,11 +1,24 @@
 const bannerreact_swiper = new Swiper('#bannerreact .swiper', {
     // Optional parameters
-    direction: 'vertical',
+    // direction: 'vertical',
     loop: true,
   
     // If we need pagination
     pagination: {
-      el: '#bannerreact .swiper-pagination',
+      el: '#bannerreact .bannerBtns',
+      bulletClass : 'bg-dark',
+      bulletActiveClass : 'active',
+      clickable :true,
+
+      renderBullet: function (index, className) {
+        switch(index){
+          case 0:text='UX전략도출 및 컨셉';break;
+          case 1:text='화면설계 및 개발 팔로업';break;
+          case 2:text='소스관리 / 바로가기';break;
+         
+        }
+        return '<span class="border text-white p-2 mx-2 ' + className + '">' + text + '</span>';
+      },
     },
   
     // Navigation arrows
@@ -23,13 +36,19 @@ const bannerreact_swiper = new Swiper('#bannerreact .swiper', {
 
   const work_swiper = new Swiper("#work .swiper", {
     pagination: {
-      el: ".swiper-pagination",
+      el: "#work .swiper-pagination",
       type: "fraction",
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+      nextEl: "#work .swiper-button-next",
+      prevEl: "#work .swiper-button-prev",
+    }
+    
+  });
+  work_swiper.on('slideChange', function () {
+    if(this.activeIndex++) {
+      addclass
+    }
   });
 
  
